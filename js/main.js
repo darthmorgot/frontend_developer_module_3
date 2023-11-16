813e6c6bcaa16a25ad1cdecba58adec0ff4a3a65
darthmorgot <dartmorgot@yandex.ru>
2023-11-16 23:51:39 +1000

    Обновил код обеих функций, добавил комментарии.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
-function getRandomInteger(from = 0, to = 10) {
+/**
+ * Функция, возвращающая случайное целое число из переданного диапазона включительно.
+ * @param from Нижняя граница диапазона
+ * @param to Верхняя граница диапазона
+ * @returns {number} Случайное целое число
+ */
+function getRandomInteger(from = 1, to = 0) {
   if (typeof from === 'string' || typeof to === 'string') {
-    return 'Необходимо ввести число';
+    throw 'Необходимо ввести число.';
   }
 
   if (from < 0 || to < 0) {
-    return 'Необходимо число больше или равное нулю';
+    throw 'Необходимо число больше или равное нулю.';
   }
 
   return Math.floor(Math.random() * (to - from + 1) + from);
 }
 
-function getRandomFloatingPointNumber(from, to) {
-  return Math.random() * (to - from + 1) + from;
+/**
+ * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+ * @param from Нижняя граница диапазона
+ * @param to Верхняя граница диапазона
+ * @param decimalPlaces Количество знаков после запятой
+ * @returns {number} Случайное число с заданным количеством знаков после запятой
+ */
+function getRandomFloatingPointNumber(from = 1, to = 0, decimalPlaces) {
+  if (typeof from === 'string' || typeof to === 'string' || typeof decimalPlaces === 'string') {
+    throw 'Необходимо ввести число.';
+  }
+
+  if (from < 0 || to < 0 || decimalPlaces < 0) {
+    throw 'Необходимо число больше или равное нулю.';
+  }
+
+  let randNum = Math.random() * (to - from) + from;
+
+  return parseFloat(randNum.toFixed(decimalPlaces));
 }
 
 getRandomInteger(2, 8);
-getRandomFloatingPointNumber(5, 9);
+getRandomFloatingPointNumber(1.567, 1.890, 2);
