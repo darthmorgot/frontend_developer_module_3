b0f9c28f16ed11f3af773e24667f01b571bb3bb9
darthmorgot <dartmorgot@yandex.ru>
2023-11-18 02:08:29 +1000

    Добавил новую функцию, проверяющую входящие параметры. Изменил код других функций, удалил проверку параметров, заменив их вызовов новой функции.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+'use strict';
+
+/**
+ * Функция, проверяющая входящие параметры
+ * @param params Массив входящих параметров
+ */
+function checkParams(...params) {
+  for (let i = 0; i < params.length; i++) {
+    if (typeof params[i] !== 'number' || Number.isNaN(params[i])) {
+      throw 'Необходимо ввести число.';
+    }
+
+    if (params[i] < 0) {
+      throw 'Необходимо число больше или равное нулю.';
+    }
+  }
+}
+
 /**
  * Функция, возвращающая случайное целое число из переданного диапазона включительно.
  * @param from Нижняя граница диапазона
@@ -5,13 +23,7 @@
  * @returns {number} Случайное целое число
  */
 function getRandomInteger(from = 1, to = 0) {
-  if (typeof from === 'string' || typeof to === 'string') {
-    throw 'Необходимо ввести число.';
-  }
-
-  if (from < 0 || to < 0) {
-    throw 'Необходимо число больше или равное нулю.';
-  }
+  checkParams(from, to);
 
   return Math.floor(Math.random() * (to - from + 1) + from);
 }
@@ -23,14 +35,8 @@ function getRandomInteger(from = 1, to = 0) {
  * @param decimalPlaces Количество знаков после запятой
  * @returns {number} Случайное число с заданным количеством знаков после запятой
  */
-function getRandomFloatingPointNumber(from = 1, to = 0, decimalPlaces) {
-  if (typeof from === 'string' || typeof to === 'string' || typeof decimalPlaces === 'string') {
-    throw 'Необходимо ввести число.';
-  }
-
-  if (from < 0 || to < 0 || decimalPlaces < 0) {
-    throw 'Необходимо число больше или равное нулю.';
-  }
+function getRandomFloatingPointNumber(from = 1, to = 0, decimalPlaces = 0) {
+  checkParams(from, to, decimalPlaces);
 
   let randNum = Math.random() * (to - from) + from;
 
