3eeb4687ccbee04231ceb8bce16e1d90ffbe67fa
darthmorgot <dartmorgot@yandex.ru>
2023-11-26 23:39:05 +1000

    Создал новый файл для хранения данных.

---
+++ js/data.js
@@ -0,0 +1,10 @@
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const HOURS = ['12:00', '13:00', '14:00'];
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+]
+
+export {TYPES, FEATURES, PHOTOS, HOURS}
