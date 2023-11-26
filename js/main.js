75201f3604c36d35c6c06b7e2976e425202dec40
darthmorgot <dartmorgot@yandex.ru>
2023-11-26 23:41:08 +1000

    Перенес часть переменных в отдельный файл, добавил импорт этих переменных, другие правки кода.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 import {getRandomInteger, getRandomFloatingPointNumber} from './randomNumbers.js';
+import {TYPES, FEATURES, PHOTOS, HOURS} from './data.js';
 
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const HOURS = ['12:00', '13:00', '14:00'];
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
-]
+const ADVERTISEMENT_COUNT = 10;
 
 const getRandomFeaturesElement = () => {
   return FEATURES[getRandomInteger(0, FEATURES.length - 1)];
@@ -21,10 +15,7 @@ const createAdvertisement = () => {
   let num = getRandomInteger(1, 10);
   let coordOne = getRandomFloatingPointNumber(35.65000, 35.70000, 5);
   let coordTwo = getRandomFloatingPointNumber(139.70000, 139.80000, 5);
-  let price = getRandomInteger(10000, 100000);
   let type = TYPES[getRandomInteger(0, TYPES.length - 1)];
-  let rooms = getRandomInteger(1, 100);
-  let guests = getRandomInteger(1, 100);
   let hour = HOURS[getRandomInteger(0, 2)];
   let features = Array.from({length: getRandomInteger(1, FEATURES.length)}, getRandomFeaturesElement);
   let photos = Array.from({length: getRandomInteger(1, PHOTOS.length)}, getRandomPhotosElement);
@@ -36,10 +27,10 @@ const createAdvertisement = () => {
     offer: {
       title: `Ads heading ${num}`,
       address: `${coordOne}, ${coordTwo}`,
-      price: price,
+      price: getRandomInteger(0, 100000),
       type: type,
-      rooms: rooms,
-      guests: guests,
+      rooms: getRandomInteger(1, 100),
+      guests: getRandomInteger(1, 100),
       checkin: hour,
       checkout: hour,
       features: [...new Set(features)],
@@ -54,6 +45,6 @@ const createAdvertisement = () => {
   }
 };
 
-const listAdvertisements = Array.from({length: 10}, createAdvertisement);
+const listAdvertisements = Array.from({length: ADVERTISEMENT_COUNT}, createAdvertisement);
 
 console.log(listAdvertisements);
