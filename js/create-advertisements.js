188726d2d6e214d3bc25e827e5d4703790a1062c
darthmorgot <dartmorgot@yandex.ru>
2023-11-29 16:05:37 +1000

    Создал новый файл, перенес в него код из файла main.js, добавил импорты из utils.js, мелкие правки кода.

---
+++ js/create-advertisements.js
@@ -0,0 +1,42 @@
+import {getRandomInteger, getRandomFloatingPointNumber, getRandomArrayElement, createArray} from './utils.js';
+import {TYPES, FEATURES, PHOTOS, HOURS} from './data.js';
+
+const getRandomFeaturesElement = () => getRandomArrayElement(FEATURES);
+
+const getRandomPhotosElement = () => getRandomArrayElement(PHOTOS);
+
+const createAdvertisement = () => {
+  let num = getRandomInteger(1, 10);
+  let coordOne = getRandomFloatingPointNumber(35.65000, 35.70000, 5);
+  let coordTwo = getRandomFloatingPointNumber(139.70000, 139.80000, 5);
+  let hour = getRandomArrayElement(HOURS);
+  let features = createArray(getRandomInteger(1, FEATURES.length), getRandomFeaturesElement);
+  let photos = createArray(getRandomInteger(1, PHOTOS.length), getRandomPhotosElement);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${num < 10 ? '0' + num : num}.png`
+    },
+    offer: {
+      title: `Ads heading ${num}`,
+      address: `${coordOne}, ${coordTwo}`,
+      price: getRandomInteger(0, 100000),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomInteger(1, 100),
+      guests: getRandomInteger(1, 100),
+      checkin: hour,
+      checkout: hour,
+      features: [...new Set(features)],
+      description: `Великолепная съемная недвижимость в центре Токио. Подходит как туристам, так и бизнесменам. Квартира полностью укомплектована и недавно отремонтирована.`,
+      photos: [...new Set(photos)]
+    },
+    location: {
+      lat: coordOne,
+      lng: coordTwo
+    }
+  }
+};
+
+const createListAdvertisements = (count) => createArray(count, createAdvertisement);
+
+export {createListAdvertisements}
