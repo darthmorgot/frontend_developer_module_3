287d228c6b604e8203be059734a3279197808aaf
darthmorgot <dartmorgot@yandex.ru>
2023-12-03 00:16:52 +1000

    Удалил предыдущий код, добавил импорт нового файла для генерации объявлений.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-import {createListAdvertisements} from './create-advertisements.js';
-
-const ADVERTISEMENT_COUNT = 10;
-
-console.log(createListAdvertisements(ADVERTISEMENT_COUNT));
+import './generate-similar-offers.js';
