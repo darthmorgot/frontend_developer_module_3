fb6186443675718baf76dd3f9e3b8101bb905c59
darthmorgot <dartmorgot@yandex.ru>
2023-12-03 23:23:45 +1000

    Добавил проверку наличия-отсутствия данных, обернул в функцию и экспортировал её.

--- js/generate-similar-offers.js
+++ js/generate-similar-offers.js
@@ -14,42 +14,73 @@ const similarOffersTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
-const listSimilarOffers = createListOffers(OFFER_COUNT);
+const createListSimilarOffers = () => {
+  const listSimilarOffers = createListOffers(OFFER_COUNT);
 
-listSimilarOffers.forEach(({author, offer, location}) => {
-  const offerElement = similarOffersTemplate.cloneNode(true);
+  listSimilarOffers.forEach(({author, offer, location}) => {
+    const offerElement = similarOffersTemplate.cloneNode(true);
 
-  const features = offerElement.querySelector('.popup__features');
-  const featureList = features.querySelectorAll('.popup__feature');
-  const modifiers = offer.features.map((param) => 'popup__feature--' + param);
+    const features = offerElement.querySelector('.popup__features');
+    const featureList = features.querySelectorAll('.popup__feature');
 
-  const photos = offerElement.querySelector('.popup__photos');
-  const photo = photos.querySelector('.popup__photo');
+    const photos = offerElement.querySelector('.popup__photos');
+    const photo = photos.querySelector('.popup__photo');
 
-  offerElement.querySelector('.popup__title').textContent = offer.title;
-  offerElement.querySelector('.popup__text--address').textContent = offer.address;
-  offerElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-  offerElement.querySelector('.popup__type').textContent = TYPES[offer.type];
-  offerElement.querySelector('.popup__text--capacity')
-    .textContent = `${offer.rooms} комнат(ы) для ${offer.guests} гостей(я)`;
-  offerElement.querySelector('.popup__text--time')
-    .textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+    offerElement.querySelector('.popup__title').textContent = offer.title;
+    offerElement.querySelector('.popup__text--address').textContent = offer.address;
 
-  featureList.forEach((featureListItem) => {
-    if (!modifiers.includes(featureListItem.classList[1])) {
-      featureListItem.remove();
+    if (offer.price) {
+      offerElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+    } else {
+      offerElement.querySelector('.popup__text--price').classList.add('hidden');
     }
+
+    offerElement.querySelector('.popup__type').textContent = TYPES[offer.type];
+
+    if (offer.rooms && offer.guests) {
+      offerElement.querySelector('.popup__text--capacity')
+        .textContent = `${offer.rooms} комнат(ы) для ${offer.guests} гостей(я)`;
+    } else {
+      offerElement.querySelector('.popup__text--capacity').classList.add('hidden');
+    }
+
+    if (offer.checkin && offer.checkout) {
+      offerElement.querySelector('.popup__text--time')
+        .textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+    } else {
+      offerElement.querySelector('.popup__text--time').classList.add('hidden');
+    }
+
+    if (offer.features) {
+      const modifiers = offer.features.map((param) => 'popup__feature--' + param);
+      featureList.forEach((featureListItem) => {
+        if (!modifiers.includes(featureListItem.classList[1])) {
+          featureListItem.remove();
+        }
+      });
+    } else {
+      features.classList.add('hidden');
+    }
+
+    if (offer.photos) {
+      offer.photos.forEach((item) => {
+        const photoClone = photo.cloneNode();
+        photoClone.src = item;
+        photos.append(photoClone);
+      });
+    }
+    photo.remove();
+
+    offerElement.querySelector('.popup__description').textContent = offer.description;
+
+    if (author.avatar) {
+      offerElement.querySelector('.popup__avatar').src = author.avatar;
+    } else {
+      offerElement.querySelector('.popup__avatar').classList.add('hidden');
+    }
+
+    entryPoint.append(offerElement);
   });
+};
 
-  offer.photos.forEach((item) => {
-    const photoClone = photo.cloneNode();
-    photoClone.src = item;
-    photos.append(photoClone);
-  });
-  photo.remove();
-
-  offerElement.querySelector('.popup__description').textContent = offer.description;
-  offerElement.querySelector('.popup__avatar').src = author.avatar;
-
-  entryPoint.append(offerElement);
-});
+export {createListSimilarOffers}
