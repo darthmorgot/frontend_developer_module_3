2d3069fad304c2c118c4be9391b88bd082791ee3
darthmorgot <dartmorgot@yandex.ru>
2023-12-05 01:07:24 +1000

    Изменил ссылки на картинки для объявлений.

--- js/data.js
+++ js/data.js
@@ -2,9 +2,12 @@ const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const HOURS = ['12:00', '13:00', '14:00'];
 const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  // 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  // 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  // 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+  'img/photos/duonguyen-8LrGtIxxa4w.jpg',
+  'img/photos/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'img/photos/claire-rendall-b6kAwr1i0Iw.jpg'
 ]
 
 export {TYPES, FEATURES, PHOTOS, HOURS}
