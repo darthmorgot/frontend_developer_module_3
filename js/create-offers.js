b6333372df340bff2d5ec9fe6510e815d71dfc33
darthmorgot <dartmorgot@yandex.ru>
2023-12-09 01:09:47 +1000

    Добавил описание функций.

--- js/create-offers.js
+++ js/create-offers.js
@@ -1,10 +1,22 @@
 import {getRandomInteger, getRandomFloatingPointNumber, getRandomArrayElement, createArray} from './utils.js';
 import {TYPES, FEATURES, PHOTOS, HOURS} from './data.js';
 
+/**
+ * Функция получает случайный элемент из массива FEATURES.
+ * @returns {*} Случайный элемент массива.
+ */
 const getRandomFeaturesElement = () => getRandomArrayElement(FEATURES);
 
+/**
+ * Функция получает случайный элемент из массива PHOTOS.
+ * @returns {*} Случайный элемент массива.
+ */
 const getRandomPhotosElement = () => getRandomArrayElement(PHOTOS);
 
+/**
+ * Функция создает объект объявления с определенным набором свойств.
+ * @returns {{offer: {features: *[], rooms: number, address: string, checkin: *, price: number, guests: number, description: string, title: string, type: *, checkout: *, photos: *[]}, author: {avatar: string}, location: {lng: number, lat: number}}} Объект объявления.
+ */
 const createOffer = () => {
   let num = getRandomInteger(1, 10);
   let coordOne = getRandomFloatingPointNumber(35.65000, 35.70000, 5);
@@ -37,6 +49,11 @@ const createOffer = () => {
   }
 };
 
+/**
+ * Функция создает массив объектов объявлений.
+ * @param count Целое число задает количество объектов в массиве.
+ * @returns {*[]} Массив объектов.
+ */
 const createListOffers = (count) => createArray(count, createOffer);
 
 export {createListOffers}
