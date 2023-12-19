852d3fd257a90c921b87d09bc195ce0e55de72d0
darthmorgot <dartmorgot@yandex.ru>
2023-12-19 23:24:56 +1000

    Удалил ручную активацию формы, добавил настройки для вывода карты на странице с активацией формы.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,46 @@ import {deactivateForm, activateForm} from './manage-form.js';
 import {renderListSimilarOffers} from './generate-similar-offers.js';
 import {formValidate} from './validate-form.js';
 
-const mapCanvas = document.querySelector('.map__canvas');
-
-mapCanvas.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  activateForm();
-}, {once: true});
-
 deactivateForm();
-renderListSimilarOffers();
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activateForm();
+  })
+  .setView({
+  lat: 35.681729,
+  lng: 139.753927,
+}, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const marker = L.marker(
+  {
+    lat: 35.681729,
+    lng: 139.753927,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon
+  },
+);
+
+marker.addTo(map);
+
+marker.on('moveend', (evt) => {
+  console.log(evt.target.getLatLng());
+});
+
+// renderListSimilarOffers();
 formValidate();
