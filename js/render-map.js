93e54b901fb8d4fd41e9979aaecdf7f220f87846
darthmorgot <dartmorgot@yandex.ru>
2023-12-28 00:16:55 +1000

    Вынес часть кода из функции renderMap, добавил в функцию renderMap обработчик клика для возврата карты в исходное состояние, добавил функцию возврата адресного маркера в исходное положение.

--- js/render-map.js
+++ js/render-map.js
@@ -4,53 +4,64 @@ import {getData} from './api.js';
 import {showAlert} from './utils.js';
 
 const addressField = document.querySelector('#address');
+const resetButton = document.querySelector('.ad-form__reset');
 
 deactivateForm();
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activateForm();
+  })
+  .setView({
+    lat: 35.681729,
+    lng: 139.753927,
+  }, 13);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const pinIcon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 35.681729,
+    lng: 139.753927,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+    riseOnHover: true,
+  },
+);
+
+/**
+ * Функция для возврата адресного маркера в исходное положение.
+ */
+const resetMainMarker = () => {
+  mainPinMarker.setLatLng({
+    lat: 35.681729,
+    lng: 139.753927,
+  });
+};
 
 /**
  * Функция для вывода интерактивной карты на страницу.
  */
 const renderMap = () => {
-  const map = L.map('map-canvas')
-    .on('load', () => {
-      activateForm();
-    })
-    .setView({
-      lat: 35.681729,
-      lng: 139.753927,
-    }, 13);
-
-  L.tileLayer(
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    },
-  ).addTo(map);
-
-  const mainPinIcon = L.icon({
-    iconUrl: './img/main-pin.svg',
-    iconSize: [52, 52],
-    iconAnchor: [26, 52],
-  });
-
-  const pinIcon = L.icon({
-    iconUrl: './img/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
-  });
-
-  const mainPinMarker = L.marker(
-    {
-      lat: 35.681729,
-      lng: 139.753927,
-    },
-    {
-      draggable: true,
-      icon: mainPinIcon,
-      riseOnHover: true,
-    },
-  );
-
   mainPinMarker.addTo(map);
 
   mainPinMarker.on('moveend', (evt) => {
@@ -87,7 +98,14 @@ const renderMap = () => {
     (err) => {
       showAlert(err);
     }
-  )
+  );
+
+  resetButton.addEventListener('click', () => {
+    const popup = document.querySelector('.leaflet-popup');
+    if (popup) popup.remove();
+
+    resetMainMarker();
+  });
 };
 
-export {renderMap};
+export {renderMap, resetMainMarker};
