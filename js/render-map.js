37a001f16e23fac3ac81435d9cb8c8e519f507b2
darthmorgot <dartmorgot@yandex.ru>
2023-12-30 00:35:55 +1000

    Добавил обработчики для формы фильтров.

--- js/render-map.js
+++ js/render-map.js
@@ -25,6 +25,8 @@ L.tileLayer(
   },
 ).addTo(map);
 
+const markerGroup = L.layerGroup().addTo(map);
+
 const mainPinIcon = L.icon({
   iconUrl: './img/main-pin.svg',
   iconSize: [52, 52],
@@ -69,6 +71,7 @@ const resetMapMainMarker = () => {
  */
 const renderMap = () => {
   const startCount = getRandomInteger(0, 40);
+
   mainPinMarker.addTo(map);
 
   mainPinMarker.on('moveend', (evt) => {
@@ -76,8 +79,6 @@ const renderMap = () => {
     const lng = evt.target.getLatLng().lng;
     addressField.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
   });
-
-  const markerGroup = L.layerGroup().addTo(map);
 
   const createMarker = (item) => {
     const {lat, lng} = item.location;
@@ -107,7 +108,72 @@ const renderMap = () => {
 
   getData(
     (offers) => {
-      offers.slice(startCount, startCount + OFFER_COUNT).forEach((item) => createMarker(item));
+      const offerSlice = offers.slice(startCount, startCount + OFFER_COUNT);
+      const selectedType = document.querySelector('#housing-type');
+      const selectedPrice = document.querySelector('#housing-price');
+      const selectedRoom = document.querySelector('#housing-rooms');
+      const selectedGuests = document.querySelector('#housing-guests');
+      const selectedFeature = document.querySelector('#housing-features');
+
+      offerSlice.forEach((item) => createMarker(item));
+
+      selectedType.addEventListener('change', () => {
+        const offerByType = offers.filter((item) => item.offer.type === selectedType.value);
+        markerGroup.clearLayers();
+        offerByType.slice(0, 10).forEach((item) => createMarker(item));
+        if (selectedType.value === 'any') offerSlice.forEach((item) => createMarker(item));
+      });
+
+      selectedPrice.addEventListener('change', () => {
+        let offerByPrice;
+        switch (selectedPrice.value) {
+          case 'middle':
+            offerByPrice = offers.filter((item) => item.offer.price >= 10000 && item.offer.price <= 50000).slice(0, 10);
+            break;
+          case 'low':
+            offerByPrice = offers.filter((item) => item.offer.price < 10000).slice(0, 10);
+            break;
+          case 'high':
+            offerByPrice = offers.filter((item) => item.offer.price > 50000).slice(0, 10);
+            break;
+          default:
+            offerByPrice = offerSlice;
+        }
+        markerGroup.clearLayers();
+        offerByPrice.forEach((item) => createMarker(item));
+      });
+
+      selectedRoom.addEventListener('change', () => {
+        const offerByRoom = offers.filter((item) => item.offer.rooms === parseInt(selectedRoom.value));
+        markerGroup.clearLayers();
+        offerByRoom.slice(0, 10).forEach((item) => createMarker(item));
+        if (selectedRoom.value === 'any') offerSlice.forEach((item) => createMarker(item));
+      });
+
+      selectedGuests.addEventListener('change', () => {
+        const offerByGuests = offers.filter((item) => item.offer.guests === parseInt(selectedGuests.value));
+        markerGroup.clearLayers();
+        offerByGuests.slice(0, 10).forEach((item) => createMarker(item));
+        if (selectedGuests.value === 'any') offerSlice.forEach((item) => createMarker(item));
+      });
+
+      selectedFeature.addEventListener('click', (evt) => {
+        let selectedFeatureValue;
+        if (evt.target.value) {
+          if (evt.target.checked) {
+            selectedFeature.setAttribute('checked', true);
+            selectedFeatureValue = evt.target.value;
+            const offerByFeature = offers.filter((item) => {
+              if (item.offer.features) return item.offer.features.indexOf(selectedFeatureValue) !== -1;
+            });
+            markerGroup.clearLayers();
+            offerByFeature.slice(0, 10).forEach((item) => createMarker(item));
+          } else {
+            markerGroup.clearLayers();
+            offerSlice.forEach((item) => createMarker(item));
+          }
+        }
+      });
     },
     (err) => {
       showAlert(err);
