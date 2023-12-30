d0ccd7c5d5c5026afaa184ae6011abb1a9b9f4d9
darthmorgot <dartmorgot@yandex.ru>
2023-12-31 01:22:29 +1000

    Вынес код фильтров объвлений в отдельный файл и добавил импорт функции из этого файла.

--- js/render-map.js
+++ js/render-map.js
@@ -2,6 +2,7 @@ import {activateForm, deactivateForm} from './manage-form.js';
 import {renderSimilarOffer} from './generate-offer.js';
 import {getData} from './api.js';
 import {showAlert, getRandomInteger} from './utils.js';
+import {filterOffer} from './filter-offer.js';
 
 const OFFER_COUNT = 10;
 const addressField = document.querySelector('#address');
@@ -109,71 +110,9 @@ const renderMap = () => {
   getData(
     (offers) => {
       const offerSlice = offers.slice(startCount, startCount + OFFER_COUNT);
-      const selectedType = document.querySelector('#housing-type');
-      const selectedPrice = document.querySelector('#housing-price');
-      const selectedRoom = document.querySelector('#housing-rooms');
-      const selectedGuests = document.querySelector('#housing-guests');
-      const selectedFeature = document.querySelector('#housing-features');
-
       offerSlice.forEach((item) => createMarker(item));
 
-      selectedType.addEventListener('change', () => {
-        const offerByType = offers.filter((item) => item.offer.type === selectedType.value);
-        markerGroup.clearLayers();
-        offerByType.slice(0, 10).forEach((item) => createMarker(item));
-        if (selectedType.value === 'any') offerSlice.forEach((item) => createMarker(item));
-      });
-
-      selectedPrice.addEventListener('change', () => {
-        let offerByPrice;
-        switch (selectedPrice.value) {
-          case 'middle':
-            offerByPrice = offers.filter((item) => item.offer.price >= 10000 && item.offer.price <= 50000).slice(0, 10);
-            break;
-          case 'low':
-            offerByPrice = offers.filter((item) => item.offer.price < 10000).slice(0, 10);
-            break;
-          case 'high':
-            offerByPrice = offers.filter((item) => item.offer.price > 50000).slice(0, 10);
-            break;
-          default:
-            offerByPrice = offerSlice;
-        }
-        markerGroup.clearLayers();
-        offerByPrice.forEach((item) => createMarker(item));
-      });
-
-      selectedRoom.addEventListener('change', () => {
-        const offerByRoom = offers.filter((item) => item.offer.rooms === parseInt(selectedRoom.value));
-        markerGroup.clearLayers();
-        offerByRoom.slice(0, 10).forEach((item) => createMarker(item));
-        if (selectedRoom.value === 'any') offerSlice.forEach((item) => createMarker(item));
-      });
-
-      selectedGuests.addEventListener('change', () => {
-        const offerByGuests = offers.filter((item) => item.offer.guests === parseInt(selectedGuests.value));
-        markerGroup.clearLayers();
-        offerByGuests.slice(0, 10).forEach((item) => createMarker(item));
-        if (selectedGuests.value === 'any') offerSlice.forEach((item) => createMarker(item));
-      });
-
-      selectedFeature.addEventListener('click', (evt) => {
-        let selectedFeatureValue;
-        if (evt.target.value) {
-          if (evt.target.checked) {
-            selectedFeature.setAttribute('checked', true);
-            selectedFeatureValue = evt.target.value;
-            const offerByFeature = offers.filter((item) => {
-              if (item.offer.features) return item.offer.features.indexOf(selectedFeatureValue) !== -1;
-            });
-            markerGroup.clearLayers();
-            offerByFeature.slice(0, 10).forEach((item) => createMarker(item));
-          } else {
-            markerGroup.clearLayers();
-            offerSlice.forEach((item) => createMarker(item));
-          }
-        }
-      });
+      filterOffer(offers, offerSlice, markerGroup, createMarker);
     },
     (err) => {
       showAlert(err);
