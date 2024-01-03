c787a00cc02aa8177080225475f89d0125b583d8
darthmorgot <dartmorgot@yandex.ru>
2024-01-04 01:44:21 +1000

    Продолжаю переделку функций фильтров, чтобы фильтрация происходила с учетом всех фильтров, а не по отдельности.

--- js/filter-offer.js
+++ js/filter-offer.js
@@ -11,7 +11,7 @@ const selectedType = formFilters.querySelector('#housing-type');
 const selectedPrice = formFilters.querySelector('#housing-price');
 const selectedRooms = formFilters.querySelector('#housing-rooms');
 const selectedGuests = formFilters.querySelector('#housing-guests');
-const selectedFeature = formFilters.querySelector('#housing-features');
+const features = formFilters.querySelectorAll('.map__checkbox');
 
 /**
  * Функция для проверки объявлений по типу жилья.
@@ -19,154 +19,153 @@ const selectedFeature = formFilters.querySelector('#housing-features');
  * @param typeValue Значение типа жилья.
  */
 const checkByType = (offer, typeValue) => {
-  return typeValue === DEFAULT_VALUE || offer.offer.type === typeValue;
+  return offer.offer.type === typeValue || typeValue === DEFAULT_VALUE;
 };
 
 /**
- * Функция для фильтрации объявлений по цене на ночь.
- * @param array Массив всех объявлений.
- * @param arraySlice Массив из 10 случайно выбранных объявлений.
- * @param layer Слой карты, куда добавляются маркеры объявлений.
- * @param cb Функция для создания и добавления маркеров на карту.
+ * Функция для проверки объявлений по цене.
+ * @param offer Объект объявления.
+ * @param priceValue Значение цены.
  */
-const checkByPrice = (array, arraySlice, layer, cb) => {
-  selectedPrice.addEventListener('change', () => {
-    let offerByPrice;
-    switch (selectedPrice.value) {
-      case 'middle':
-        offerByPrice = array.filter((item) => item.offer.price >= PRICES.middle && item.offer.price <= PRICES.high).slice(0, 10);
-        break;
-      case 'low':
-        offerByPrice = array.filter((item) => item.offer.price < PRICES.middle).slice(0, 10);
-        break;
-      case 'high':
-        offerByPrice = array.filter((item) => item.offer.price > PRICES.high).slice(0, 10);
-        break;
-      default:
-        offerByPrice = arraySlice;
-    }
-    layer.clearLayers();
-    offerByPrice.forEach((item) => cb(item));
-  });
+const checkByPrice = (offer, priceValue) => {
+  switch (priceValue) {
+    case 'middle':
+      return offer.offer.price >= PRICES.middle && offer.offer.price <= PRICES.high;
+    case 'low':
+      return offer.offer.price < PRICES.middle;
+    case 'high':
+      return offer.offer.price > PRICES.high;
+    default:
+      return true;
+  }
 };
 
 /**
- * Функция для фильтрации объявлений по количеству комнат.
- * @param array Массив всех объявлений.
- * @param arraySlice Массив из 10 случайно выбранных объявлений.
- * @param layer Слой карты, куда добавляются маркеры объявлений.
- * @param cb Функция для создания и добавления маркеров на карту.
+ * Функция для проверки объявлений по количеству комнат.
+ * @param offer Объект объявления.
+ * @param roomValue Количество комнат.
  */
-const checkByRooms = (array, arraySlice, layer, cb) => {
-  selectedRooms.addEventListener('change', () => {
-    const offerByRoom = array.filter((item) => item.offer.rooms === parseInt(selectedRooms.value));
-    layer.clearLayers();
-    offerByRoom.slice(0, 10).forEach((item) => cb(item));
-    if (selectedRooms.value === 'any') arraySlice.forEach((item) => cb(item));
-  });
+const checkByRooms = (offer, roomValue) => {
+  return offer.offer.rooms === +roomValue || roomValue === DEFAULT_VALUE;
 };
 
 /**
- * Функция для фильтрации объявлений по количеству гостей.
- * @param array Массив всех объявлений.
- * @param arraySlice Массив из 10 случайно выбранных объявлений.
- * @param layer Слой карты, куда добавляются маркеры объявлений.
- * @param cb Функция для создания и добавления маркеров на карту.
+ * Функция для проверки объявлений по количеству гостей.
+ * @param offer Объект объявления.
+ * @param guestValue Количество гостей.
  */
-const checkByGuests = (array, arraySlice, layer, cb) => {
-  selectedGuests.addEventListener('change', () => {
-    const offerByGuests = array.filter((item) => item.offer.guests === parseInt(selectedGuests.value));
-    layer.clearLayers();
-    offerByGuests.slice(0, 10).forEach((item) => cb(item));
-    if (selectedGuests.value === 'any') arraySlice.forEach((item) => cb(item));
-  });
+const checkByGuests = (offer, guestValue) => {
+  return offer.offer.guests === +guestValue || guestValue === DEFAULT_VALUE;
 };
 
-/**
- * Функция для фильтрации объявлений по типу и количеству удобств.
- * @param array Массив всех объявлений.
- * @param arraySlice Массив из 10 случайно выбранных объявлений.
- * @param layer Слой карты, куда добавляются маркеры объявлений.
- * @param cb Функция для создания и добавления маркеров на карту.
- */
-const checkByFeature = (array, arraySlice, layer, cb) => {
-  let featureList = [];
-  let checkedFeature = [];
+// /**
+//  * Функция для фильтрации объявлений по типу и количеству удобств.
+//  * @param array Массив всех объявлений.
+//  * @param arraySlice Массив из 10 случайно выбранных объявлений.
+//  * @param layer Слой карты, куда добавляются маркеры объявлений.
+//  * @param cb Функция для создания и добавления маркеров на карту.
+//  */
+// const checkByFeature = (array, arraySlice, layer, cb) => {
+//   let featureList = [];
+//   let checkedFeature = [];
+//
+//   /**
+//    * Функция для вычисления веса объявления по количеству удобств.
+//    * @param offer Объект объявления.
+//    * @returns {number} Вес объявления.
+//    */
+//   const getOfferRank = (offer) => {
+//     let rank = 0;
+//
+//     for (const feature of featureList) {
+//       if (offer.offer.features && offer.offer.features.includes(feature)) {
+//         rank += 1;
+//       }
+//     }
+//
+//     return rank;
+//   };
+//
+//   /**
+//    * Функция для сравнения веса объявлений.
+//    * @param offerA Первое объявление.
+//    * @param offerB Второе объявление.
+//    * @returns {number} Число, указывающее место объявления в массиве.
+//    */
+//   const compareOffers = (offerA, offerB) => {
+//     const rankA = getOfferRank(offerA);
+//     const rankB = getOfferRank(offerB);
+//
+//     return rankB - rankA;
+//   };
+//
+//   selectedFeature.addEventListener('change', (evt) => {
+//     const offerByFeature = array.slice();
+//     if (evt.target.value) {
+//       if (evt.target.checked) {
+//         selectedFeature.setAttribute('checked', 'true');
+//         featureList.push(evt.target.value);
+//         checkedFeature.push('check');
+//
+//         layer.clearLayers();
+//         offerByFeature.sort(compareOffers).slice(0, 10).forEach((item) => cb(item));
+//       } else if (!evt.target.checked) {
+//         checkedFeature.pop();
+//       }
+//       if (checkedFeature.length === 0) {
+//         layer.clearLayers();
+//         arraySlice.forEach((item) => cb(item));
+//       }
+//     }
+//   });
+// };
 
-  /**
-   * Функция для вычисления веса объявления по количеству удобств.
-   * @param offer Объект объявления.
-   * @returns {number} Вес объявления.
-   */
-  const getOfferRank = (offer) => {
-    let rank = 0;
-
-    for (const feature of featureList) {
-      if (offer.offer.features && offer.offer.features.includes(feature)) {
-        rank += 1;
-      }
-    }
-
-    return rank;
-  };
-
-  /**
-   * Функция для сравнения веса объявлений.
-   * @param offerA Первое объявление.
-   * @param offerB Второе объявление.
-   * @returns {number} Число, указывающее место объявления в массиве.
-   */
-  const compareOffers = (offerA, offerB) => {
-    const rankA = getOfferRank(offerA);
-    const rankB = getOfferRank(offerB);
-
-    return rankB - rankA;
-  };
-
-  selectedFeature.addEventListener('change', (evt) => {
-    const offerByFeature = array.slice();
-    if (evt.target.value) {
-      if (evt.target.checked) {
-        selectedFeature.setAttribute('checked', 'true');
-        featureList.push(evt.target.value);
-        checkedFeature.push('check');
-
-        layer.clearLayers();
-        offerByFeature.sort(compareOffers).slice(0, 10).forEach((item) => cb(item));
-      } else if (!evt.target.checked) {
-        checkedFeature.pop();
-      }
-      if (checkedFeature.length === 0) {
-        layer.clearLayers();
-        arraySlice.forEach((item) => cb(item));
-      }
-    }
-  });
+const checkByFeature = (offer, featureValue) => {
+  if (offer.offer.features) {
+    return offer.offer.features.includes(featureValue);
+  }
 };
 
 const getCheckedOffers = (array) => {
-  const checkedOffers = [];
+  let checkedOffers = [];
+  let featureList = [];
+  for (let feature of features) {
+    if (feature.checked) {
+      featureList.push(feature.value);
+    }
+  }
 
   for (let arrayElement of array) {
-    if (checkByType(arrayElement, selectedType.value)) {
+    if (
+      checkByType(arrayElement, selectedType.value) &&
+      checkByPrice(arrayElement, selectedPrice.value) &&
+      checkByRooms(arrayElement, selectedRooms.value) &&
+      checkByGuests(arrayElement, selectedGuests.value)
+    ) {
       checkedOffers.push(arrayElement);
     }
   }
+  if (checkedOffers.length === array.length) {
+    checkedOffers = [];
+  }
+  console.log(checkedOffers);
 
   return checkedOffers;
 };
 
 const filterByAll = (array, arraySlice, layer, cb) => {
   formFilters.addEventListener('change', debounce(() => {
+    let arrCheckedOffers = getCheckedOffers(array);
     layer.clearLayers();
-    getCheckedOffers(array).slice(0, 10).forEach((item) => cb(item));
+    if (arrCheckedOffers.length !== 0) {
+      arrCheckedOffers.slice(0, 10).forEach((item) => cb(item));
+    } else {
+      arrCheckedOffers.slice(0, 10).forEach((item) => cb(item));
+      // arraySlice.forEach((item) => cb(item));
+    }
   }));
 
-  // checkByType(array, arraySlice, layer, cb);
-  checkByPrice(array, arraySlice, layer, cb);
-  checkByRooms(array, arraySlice, layer, cb);
-  checkByGuests(array, arraySlice, layer, cb);
-  checkByFeature(array, arraySlice, layer, cb);
+  // checkByFeature(array, arraySlice, layer, cb);
 };
 
 export {filterByAll};
